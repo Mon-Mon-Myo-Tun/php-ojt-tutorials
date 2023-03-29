ea611759cb01c532e1d9ffd8d598d2bd110769b1
MonMonMyoTun14 <monmon.myotun@gmai.com>
2023-03-29 10:25:45 +0630

    Initial commit

---
+++ Tutorial_01/index.php
@@ -0,0 +1,73 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ChessBoard </title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/reset.css">
+</head>
+
+<body>
+<div class="chess">
+<h2> ChessBoard</h2>
+
+<div class="table1">
+<table class="tb">
+  <?php
+
+  function drawChessBorad($rows, $cols)
+  {
+      if (!$rows >0 || !$cols > 0) {
+          if (!$rows >0 && !$cols> 0) {
+              echo "rows and cols parameters must be greater than 0.";
+          } elseif (!$rows>0 &&  !is_int($cols)) {
+              echo "rows must be greater than 0 and colums must be integer";
+          } elseif (!is_int($rows) && !$cols>0) {
+              echo "rows must be number and colums must be greater than 0";
+          } elseif (!$rows >0) {
+              echo "row must be greater than 0";
+          } elseif (!$cols >0) {
+              echo "columns must be greater than 0";
+          }
+      } elseif (!is_int($rows) || !is_int($cols)) {
+          if (!is_int($rows) && !is_int($cols)) {
+              echo "rows and colums must be number.";
+          } elseif (!is_int($rows)) {
+              echo "rows must be  number";
+          } elseif (!is_int($cols)) {
+              echo "colums must be  number";
+          }
+      } elseif ($rows && $cols || $rows==$cols) {
+        echo $rows ."rows and" .$cols ."colums chess borad.";
+          for ($i = 1; $i <= $rows; $i++) {
+              echo"<tr>";
+
+              for ($j = 1; $j <= $cols; $j++) {
+                  if (($i + $j) % 2 == 0) {
+                      $color="black";
+                  } else {
+                      $color="white";
+                  } echo " <td class='chess-blk {$color}' ></td>";
+              }
+              // Add a line break after each row
+          } echo"</tr>";
+         
+      }
+  }
+
+  drawChessBorad(8,8);
+
+  ?>
+ 
+  </table>
+
+</div>
+
+
+</div>
+</body>
+</html>
