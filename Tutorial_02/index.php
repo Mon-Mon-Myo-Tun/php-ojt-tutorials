112afe8c69a2241d704474e26c55d1a1576fc5c1
MonMonMyoTun14 <monmon.myotun@gmai.com>
2023-03-30 16:08:39 +0630

    added index.php

--- Tutorial_02/index.php
+++ Tutorial_02/index.php
@@ -10,47 +10,50 @@
 </head>
 <body>
     <div class="diamond">
-    <h2>Diamond Pattern</h2>
-<div class="pattern">
-<?php
-
-function makeDiamondShape($row)
-{
-    if(!is_int($row)) {
-        
-        echo "Error: Input must be an integer.";
-    }else if
-     ($row % 2 == 0) {
-        echo "must be positive odd number";
-        $row++;
-    } 
-    else{
-        $n=($row+1)/2;
-        $m=0;
-    for($i=1;$i<=$row;$i++){
-        $i<=$n ? $m++ : $m--;
-        for($j=1;$j<=$row;$j++){
-            if(($j >= $n+1-$m ) && ($j<=$n-1+$m)){
-                echo "*";
-            }else {
-                echo "&nbsp; ";
-            }
-           
-        } echo "<br>";
-    }
-
-    }
-    
-    
-    
-}
-
-makeDiamondShape(3);
-
-?>
-
-</div>
+        <h2>Diamond Pattern</h2>
+        <div class="pattern">
+            <?php
+                /**
+                 * Show the application dashboard
+                 *
+                 * @param $row
+                 */
+                function makeDiamondShape($row)
+                {
+                    if(!is_int($row)) 
+                    {
+                        echo "Error: Input must be an integer.";
+                    }else if($row % 2 == 0) 
+                    {
+                        echo "must be  positive odd number";
+                    
+                    } elseif($row <= 0)
+                    {
+                        echo "must be positive number";
+                    }
+                    else
+                    {
+                        $n=($row+1)/2;//2
+                        $m=0;
+                        for($i=1;$i<=$row;$i++)
+                        {
+                            $i<=$n ? $m++ : $m--;
+                            for($j=1;$j<=$row;$j++)
+                        {
+                            if(($j >= $n+1-$m ) && ($j<=$n-1+$m))
+                            {
+                                echo "*";
+                            }else 
+                            {
+                                echo " &nbsp; ";
+                            }
+                        }   echo "<br>";
+                        }
+                    } 
+                }
+                makeDiamondShape(7);
+            ?>
+        </div>
     </div>
-
 </body>
 </html>
