f32e3e1857197d35e4622e30b07598a70be19060
Mon-Mon-Myo-Tun <128568062+Mon-Mon-Myo-Tun@users.noreply.github.com>
2023-04-05 16:14:25 +0630

    Update array different

--- Tutorial_04/array_diff.php
+++ Tutorial_04/array_diff.php
@@ -1,26 +1,26 @@
 <?php
+/* 
+    * Compares the values of two (or more) arrays, and returns the differences.
+    * 
+    * @param $arr1,$arr2
 
-$a = [1,2];
-$b = [1];
+*/
+    $arr_one = [1,2];
+    $arr_two = [1];
 
-function arrayDiff($arr1, $arr2) {
-  $result = [];
-  foreach ($arr1 as $var_a) {
-    if (!in_array($var_a, $arr2)) {
-      $result[] = $var_a;
+    function arrayDiff($arr1, $arr2) 
+    {
+        $result = [];
+        foreach ($arr1 as $var_a) 
+        {
+            if (!in_array($var_a, $arr2))
+            {
+                $result[] = $var_a;
+            }
+        }
+        return json_encode($result);
     }
-  }
-  return json_encode($result);
-}
 
-$result = arrayDiff($a, $b);
-echo $result;
-
+    $result = arrayDiff( $arr_one,$arr_two);
+    echo $result;
 ?>
-<!--// Result
-// arrayDiff([1, 2], [1]); // output => [2]
-// arrayDiff([1, 2, 2], [1]); // output => [2, 2]
-// arrayDiff([1, 2, 2], [2]); // output => [1]
-// arrayDiff([1, 2, 2], []); // output => [1, 2, 2]
-// arrayDiff([], [1, 2]); // output => []
-// arrayDiff([1, 2, 3], [1, 2]); // output => [3]-->
