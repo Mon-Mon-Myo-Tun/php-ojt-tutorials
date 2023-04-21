641971582804a73ff12a1ce77ef99b22863107f8
MonMonMyoTun14 <monmon.myotun@gmai.com>
2023-04-21 13:31:38 +0630

    Laravel_QuickStart  upload

---
+++ Laravel_QuickStart/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Laravel Quickstart - Basic</title>
+        <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" href="{{asset('fontawesome/all.min.css')}}">
+    </head>
+    <body>
+        <div class="container border border-1">
+            <nav class="navbar">
+                Task List
+            </nav>
+        </div>
+        @yield('content')
+    </body>
+    <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('fontawesome/all.min.js')}}"></script>
+</html>
---
+++ Laravel_QuickStart/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+Route::get('/', function () {
+    return view('welcome');
+    
+});
+
+Route::get('/',[TaskController::class,'show'])->name('show#list');
+Route::post('/taskList',[TaskController::class,'task'])->name('task#list');
+Route::delete('/taskList/{tasks}',[TaskController::class,'remove'])->name('remove#list');
