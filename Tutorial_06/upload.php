c4d8fbe2c9b55e39aa152dd602d0cb4b2207f31f
MonMonMyoTun14 <monmon.myotun@gmai.com>
2023-04-26 15:48:12 +0630

    update tutorial06 review

--- Tutorial_06/upload.php
+++ Tutorial_06/upload.php
@@ -1,7 +1,11 @@
 <?php
-    if (isset($_POST['upload'])) {
-        if (empty($_POST['foldername'])) {
-            $errorName= "Folder name field is required...";
+    $errorName = $errorfilename = $errorfilesize = $folder_name = "";
+    
+    if (isset($_POST['upload'])) 
+    {
+        if (empty($_POST['foldername'])) 
+        {
+            $errorName = "Folder name field is required...";
         } 
         else 
         {
@@ -15,27 +19,34 @@
         else
         {
             $image_name = $_FILES['filename']['name'];
-          
             $image_temp = $_FILES['filename']['tmp_name'];
             $upload_path = "images/" . $folder_name . "/" . $image_name;
             $image_info = getimagesize($image_temp);
-            if (!file_exists( $folder_name))
+
+            if ($_FILES['filename']['size'] > 20000) 
             {
-                mkdir("images/" . $folder_name, 0777, true);
-                
+                echo '<script>alert("File size should not exceed 2000 bytes");</script>';
             }
-            if ($image_info === false) 
+            else 
             {
-                $error_msg = " Please upload an image file.";
-            } 
-            else if (move_uploaded_file($image_temp, $upload_path)  && !empty($_POST['foldername']))
-            {
-                $success_msg = "Uploaded image successfully.";
-            } else 
-            {
-                $error_msg = "Error uploading file.You need to add folder!";
+                if (!file_exists("images/" . $folder_name)) 
+                {
+                    mkdir("images/" . $folder_name, 0777, true);
+                } 
+
+                if (file_exists($upload_path)) 
+                {
+                    $errorfilename = "File with the same name already exists in the folder...";
+                } 
+                else if (move_uploaded_file($image_temp, $upload_path) && !empty($_POST['foldername'])) 
+                {
+                    $success_msg = "Uploaded image successfully.";
+                } 
+                else 
+                {
+                    $error_msg = "Error uploading file. You need to add folder!";
+                }
             }
         }
-    }       
+    }     
 ?>
-
